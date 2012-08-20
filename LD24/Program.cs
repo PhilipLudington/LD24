e216db6ada543acd66641542ed68978e4775accd
Philip Ludington <mrphil@mrphilgames.com>
2012-08-19 22:38:14 -0400

    Dropping al lthe Unity stuff, and going with C# + SFML

---
+++ LD24/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LD24.MrPhil
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
