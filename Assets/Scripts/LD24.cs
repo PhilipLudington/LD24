a564c7e996f870ce2e214163797e6886dd164500
Philip Ludington <mrphil@mrphilgames.com>
2012-08-25 12:54:42 -0400

    Getting rendering working, almost there.

--- Assets/Scripts/LD24.cs
+++ Assets/Scripts/LD24.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System;
 
 public class LD24 : MonoBehaviour
 {
@@ -12,10 +13,23 @@ public class LD24 : MonoBehaviour
         futileParams.AddResolutionLevel(1024, 1, 1, "");
 
         futileParams.origin = new Vector2(0, 0);
+        futileParams.shouldLerpToNearestResolutionLevel = false;
 
         Futile.instance.Init(futileParams);
+        Futile.screen.SignalResize += new System.Action<bool>(screen_SignalResize);
+        screen_SignalResize(true);
 
         Futile.atlasManager.LoadAtlas("Atlases/Atlas-1");
+
+        FSprite sprite3 = new FSprite("Background.png");
+        sprite3.anchorX = 0;
+        sprite3.anchorY = sprite3.height;
+        sprite3.x = 0;
+        sprite3.y = Futile.screen.height;
+        sprite3.scaleX = Futile.screen.width;
+        sprite3.scaleY = Futile.screen.height;
+
+        Futile.stage.AddChild(sprite3);
 
         FSprite sprite = new FSprite("Ludum-Dare.png");
         sprite.anchorX = 0;
@@ -32,6 +46,20 @@ public class LD24 : MonoBehaviour
         sprite2.y = Futile.screen.height - sprite2.height;
 
         Futile.stage.AddChild(sprite2);
+
+        FSprite sprite4 = new FSprite("Brick.png");
+        sprite4.anchorX = 0;
+        sprite4.anchorY = 0;
+        sprite4.x = 50;
+        sprite4.y = 50;
+
+        Futile.stage.AddChild(sprite2);
+    }
+
+    void screen_SignalResize(bool obj)
+    {
+        Futile.stage.scale = Math.Max(1.0f, Math.Min(Futile.screen.width / 1024, Futile.screen.height / 768));
+        Debug.Log("Screen Resized!");
     }
 
     // Update is called once per frame
