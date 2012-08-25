e6a9dd7bd4cc2451ec535c64bd95d9e98aeecbd4
Philip Ludington <mrphil@mrphilgames.com>
2012-08-25 17:00:37 -0400

    Move player with arrow keys

--- Assets/Scripts/LD24.cs
+++ Assets/Scripts/LD24.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class LD24 : MonoBehaviour
 {
-    List<FSprite> bricks = new List<FSprite>();
-    FContainer fContainerMain = new FContainer();
-
+    private List<FSprite> bricks = new List<FSprite>();
+    private FContainer fContainerMain = new FContainer();
+    private FSprite player;
+    private float playerSpeed = 40.0f;
 
     // Use this for initialization
     void Start()
@@ -22,36 +23,58 @@ public class LD24 : MonoBehaviour
         //Futile.screen.SignalResize += new System.Action<bool>(screen_SignalResize);
         //screen_SignalResize(true);
 
+        Futile.stage.AddChild(fContainerMain);
+
         Futile.atlasManager.LoadAtlas("Atlases/Atlas-1");
 
         FSprite sprite3 = new FSprite("Backgroundx4.png");
         sprite3.x = Futile.screen.halfWidth;
         sprite3.y = Futile.screen.halfHeight;
-        sprite3.scale = 256;
+        sprite3.scale = 500;
 
-        Futile.stage.AddChild(sprite3);
+        fContainerMain.AddChild(sprite3);
 
-        FSprite sprite = new FSprite("Ludum-Dare.png");
-        sprite.anchorX = 0;
-        sprite.anchorY = 0;
-        sprite.x = 10;
-        sprite.y = 10;
+        //FSprite sprite = new FSprite("Ludum-Dare.png");
+        //sprite.anchorX = 0;
+        //sprite.anchorY = 0;
+        //sprite.x = 10;
+        //sprite.y = 10;
 
-        Futile.stage.AddChild(sprite);
+        //fContainerMain.AddChild(sprite);
 
-        FSprite sprite2 = new FSprite("MrPhilGamesLogo737.png");
-        sprite2.anchorX = 0;
-        sprite2.anchorY = 0;
-        sprite2.x = 0;
-        sprite2.y = Futile.screen.height - sprite2.height;
+        //FSprite sprite2 = new FSprite("MrPhilGamesLogo737.png");
+        //sprite2.anchorX = 0;
+        //sprite2.anchorY = 0;
+        //sprite2.x = 0;
+        //sprite2.y = Futile.screen.height - sprite2.height;
 
-        Futile.stage.AddChild(sprite2);
+        //fContainerMain.AddChild(sprite2);
 
-        FSprite sprite4 = new FSprite("Brick.png");
-        sprite4.x = 50;
-        sprite4.y = 50;
+        player = new FSprite("Brick.png");
+        player.anchorX = 0;
+        player.anchorY = 0;
+        player.x = 25;
+        player.y = 42;
 
-        Futile.stage.AddChild(sprite4);
+        fContainerMain.AddChild(player);
+
+        for (int x = 25; x + 40 < 1024; x += 44)
+        {
+            for (int y = 14 * 4; y + 10 < 768 - (14 * 1); y += 14)
+            {
+                FSprite brick = new FSprite("Brick.png");
+                brick.anchorX = 0;
+                brick.anchorY = 0;
+                brick.color = new Color(UnityEngine.Random.Range(0, 100) / 100.0f,
+                    UnityEngine.Random.Range(0, 100) / 100.0f,
+                    UnityEngine.Random.Range(0, 100) / 100.0f, 1);
+                brick.x = x;
+                brick.y = y;
+
+                bricks.Add(brick);
+                fContainerMain.AddChild(brick);
+            }
+        }
     }
 
     void screen_SignalResize(bool obj)
@@ -63,6 +86,7 @@ public class LD24 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        player.x += Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+        player.y += Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
     }
 }
---
+++ TODO.md
@@ -0,0 +1,5 @@
+TODO
+----
+X Move player with arrow keys
+* Collision between player and bricks
+* timer ot destory bricks
