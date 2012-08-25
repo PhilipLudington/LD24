7ef630d3d761c79121b98fda1daa1da03e4d0800
Philip Ludington <mrphil@mrphilgames.com>
2012-08-25 18:53:00 -0400

    Player-Brick collision fixed.

--- Assets/Scripts/LD24.cs
+++ Assets/Scripts/LD24.cs
@@ -24,8 +24,8 @@ public class LD24 : MonoBehaviour
         futileParams.shouldLerpToNearestResolutionLevel = false;
 
         Futile.instance.Init(futileParams);
-        //Futile.screen.SignalResize += new System.Action<bool>(screen_SignalResize);
-        //screen_SignalResize(true);
+        Futile.screen.SignalResize += new System.Action<bool>(screen_SignalResize);
+        screen_SignalResize(true);
 
         Futile.stage.AddChild(fContainerMain);
 
@@ -37,22 +37,6 @@ public class LD24 : MonoBehaviour
         sprite3.scale = 500;
 
         fContainerMain.AddChild(sprite3);
-
-        //FSprite sprite = new FSprite("Ludum-Dare.png");
-        //sprite.anchorX = 0;
-        //sprite.anchorY = 0;
-        //sprite.x = 10;
-        //sprite.y = 10;
-
-        //fContainerMain.AddChild(sprite);
-
-        //FSprite sprite2 = new FSprite("MrPhilGamesLogo737.png");
-        //sprite2.anchorX = 0;
-        //sprite2.anchorY = 0;
-        //sprite2.x = 0;
-        //sprite2.y = Futile.screen.height - sprite2.height;
-
-        //fContainerMain.AddChild(sprite2);
 
         player = new FSprite("Brick.png");
         player.anchorX = 0;
@@ -66,21 +50,32 @@ public class LD24 : MonoBehaviour
         {
             for (int y = 14 * 4; y + 10 < 768 - (14 * 1); y += 14)
             {
-                FSprite brick = new FSprite("Brick.png");
-                brick.anchorX = 0;
-                brick.anchorY = 0;
-                brick.color = new Color(UnityEngine.Random.Range(0, 100) / 100.0f,
-                    UnityEngine.Random.Range(0, 100) / 100.0f,
-                    UnityEngine.Random.Range(0, 100) / 100.0f, 1);
-                brick.x = x;
-                brick.y = y;
-
-                bricks.Add(brick);
-                fContainerMain.AddChild(brick);
+                AddBrick(x, y);
             }
         }
 
-        Debug.Log(bricks.Count);
+        // AddBrick(player.x, player.y + 15);
+
+        // AddLogos();
+    }
+
+    private void AddBrick(float x, float y)
+    {
+        FSprite brick = new FSprite("Brick.png");
+        brick.anchorX = 0;
+        brick.anchorY = 0;
+        brick.color = new Color(UnityEngine.Random.Range(0, 100) / 100.0f,
+            UnityEngine.Random.Range(0, 100) / 100.0f,
+            UnityEngine.Random.Range(0, 100) / 100.0f, 1);
+        brick.x = x;
+        brick.y = y;
+
+        bricks.Add(brick);
+        fContainerMain.AddChild(brick);
+    }
+    private void AddBrick(int x, int y)
+    {
+        AddBrick((float)x, (float)y);
     }
 
     void screen_SignalResize(bool obj)
@@ -95,13 +90,12 @@ public class LD24 : MonoBehaviour
         // Did we bump into a brick?
         foreach (FSprite brick in bricks)
         {
-            Rect rectBrick = brick.textureRect.CloneAndMultiply(brick.scale).CloneAndOffset(brick.x, brick.y);
-
-            if (player.textureRect.CloneAndMultiply(player.scale).CheckIntersect(rectBrick))
+            Rect rectBrick = brick.textureRect.CloneAndOffset(brick.x, brick.y);
+            Rect rectPlayer = player.textureRect.CloneAndOffset(player.x, player.y);
+            if (rectPlayer.CheckIntersect(rectBrick))
             {
                 playerSpeedY = 0;
                 playerSpeedX = 0;
-                Debug.Log("Bump");
             }
         }
 
@@ -207,5 +201,25 @@ public class LD24 : MonoBehaviour
 
         // Move along Y at our speed
         player.y += playerSpeedY;
+    }
+
+    private void AddLogos()
+    {
+
+        FSprite sprite = new FSprite("Ludum-Dare.png");
+        sprite.anchorX = 0;
+        sprite.anchorY = 0;
+        sprite.x = 10;
+        sprite.y = 10;
+
+        fContainerMain.AddChild(sprite);
+
+        FSprite sprite2 = new FSprite("MrPhilGamesLogo737.png");
+        sprite2.anchorX = 0;
+        sprite2.anchorY = 0;
+        sprite2.x = 0;
+        sprite2.y = Futile.screen.height - sprite2.height;
+
+        fContainerMain.AddChild(sprite2);
     }
 }
